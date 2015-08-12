4d3141720ad846e88065f80bfa9bccca4f1c55ca
Olaf Buddenhagen <antrik@users.sf.net>
2015-08-12 03:45:12 +0200

    Add copyright/license notices and GPL text

--- udffindfile.c
+++ udffindfile.c
@@ -1,4 +1,24 @@
 /*
+ udffindfile -- find offset of a file in a DVD UDF/ISO filesystem image.
+ (Helper program distributed as part of isoterik.)
+
+ Copyright (C) 2015  Olaf Buddenhagen <antrik@users.sf.net>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * Helper program using libdvdread to find the offset (in 2 KiB UDF blocks)
  * of a file in a DVD UDF/ISO filesystem image.
  *
